8854fabee30032d0f09bf6eb33bb451aaa655749
Sarah Abasov <sarah.abasov@gmail.com>
2016-04-15 14:55:57 -0500

    sabasov_cacheMatrix_assignment
    Answer code for course 2 week 3 assignment
    Sarah Abasov

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions save computation time by caching the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix stores the matrix and its inverse (once calcualated) in a list
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks for a cached answer (matrix inverse) and if it doesn't find one calculates and returns the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
